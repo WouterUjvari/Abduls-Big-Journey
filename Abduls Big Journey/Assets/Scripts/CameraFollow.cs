e88b648e62e0743338b284b417a7e9d246d68bd7
Dominik Beens <dominik.beens@hotmail.nl>
2017-09-12 13:22:17 +0200

    camera follow

---
+++ Abduls Big Journey/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject target;
+
+    public Vector3 offset;
+    public float followSpeed = 5f;
+
+    private void Awake()
+    {
+        offset = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, followSpeed * Time.deltaTime);
+        transform.LookAt(target.transform);
+    }
+}
