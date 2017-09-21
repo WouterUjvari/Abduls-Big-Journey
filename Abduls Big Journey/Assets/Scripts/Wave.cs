a45a8786201048a322aff133950766d0dd95c65c
Dominik Beens <dominik.beens@hotmail.nl>
2017-09-21 11:45:26 +0200

    wave

---
+++ Abduls Big Journey/Assets/Scripts/Wave.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+
+    private Transform waveGraphic;
+    private Quaternion waveGraphicRotation;
+
+    public float waveRotationSpeed;
+
+    private void Awake()
+    {
+        waveGraphic = transform.GetChild(0);
+
+        waveGraphicRotation = waveGraphic.rotation;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(-Vector3.forward * (Time.deltaTime * waveRotationSpeed));
+
+        waveGraphic.rotation = waveGraphicRotation;
+    }
+}
