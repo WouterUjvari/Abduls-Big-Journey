418c3ea081acddebb27ea746f730bee2d3ec5f7d
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-09-23 23:56:25 +0200

    New Sea

---
+++ Abduls Big Journey/Assets/Scripts/EmitWave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitWave : MonoBehaviour {
+
+    public List<GameObject> waveList = new List<GameObject>();
+    public float timer;
+    public float frequency;
+
+    private void FixedUpdate()
+    {
+        timer++;
+        if (timer >= frequency)
+        {
+            timer = 0;
+            print("Wave");
+            Instantiate(waveList[Random.Range(0, 4)], transform);
+        }
+    }
+}
---
+++ Abduls Big Journey/Assets/Scripts/WaveMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMove : MonoBehaviour {
+
+    public float speedModifier = 1;
+    private float timer;
+    public float lifetime = 1000;
+
+    private void FixedUpdate()
+    {    
+        transform.Translate(speedModifier * 0.5f, 0, 0);
+
+        timer++;
+        if (timer >= lifetime)
+        {
+            timer = 0;
+            Destroy(gameObject);
+        }
+    }
+}
