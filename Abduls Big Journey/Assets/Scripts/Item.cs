b345e52d50709377377d1cc7fa174e792f28690e
Dominik Beens <dominik.beens@hotmail.nl>
2017-09-26 14:03:15 +0200

    squid in level, exploding heineken

--- Abduls Big Journey/Assets/Scripts/Item.cs
+++ Abduls Big Journey/Assets/Scripts/Item.cs
@@ -8,11 +8,16 @@ public class Item : MonoBehaviour
         PlayerItem,
         EnemyItem
     }
+    [Header("Item Properties")]
     public Type type;
 
     public int damage;
 
     public float rotateSpeed;
+
+    [Header("Extras")]
+    public GameObject itemToSpawnOnDestroy;
+    public int amountOfItemsToSpawnOnDestroy;
 
     private void Update()
     {
@@ -26,6 +31,13 @@ public class Item : MonoBehaviour
             if (other.tag == "Enemy")
             {
                 other.GetComponent<Enemy>().Hit(damage);
+
+                for (int i = 0; i < amountOfItemsToSpawnOnDestroy; i++)
+                {
+                    GameObject itemSpawn = Instantiate(itemToSpawnOnDestroy, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+                    itemSpawn.GetComponent<Rigidbody>().AddForce(Vector3.up * Random.Range(200, 300) + Vector3.right * Random.Range(-200, 200));
+                }
+
                 Destroy(gameObject);
             }
         }
