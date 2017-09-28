4cc09cfbe7a4f20475b2a530dd2e58df42c1e669
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-09-28 11:03:01 +0200

    Wind implemented

---
+++ Abduls Big Journey/Assets/Scripts/Cloud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+    public GameObject cloudOrange;
+    public GameObject cloudBlue;
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        
+        if (other.gameObject.tag == "CloudOrange")
+        {          
+            gameObject.transform.position = cloudBlue.transform.GetChild(0).transform.position;
+            
+        }
+
+        if (other.gameObject.tag == "CloudBlue")
+        {
+            gameObject.transform.position = cloudOrange.transform.GetChild(0).transform.position;
+        }
+    }
+
+     
+}
---
+++ Abduls Big Journey/Assets/Scripts/Wind.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+    public GameObject cloudOrange;
+    public GameObject cloudBlue;
+
+    public Vector2 windDirectionAndStrenght = new Vector2(0, -9.81f);
+
+    void LateUpdate()
+    {
+        Physics2D.gravity = windDirectionAndStrenght;
+    }
+}
