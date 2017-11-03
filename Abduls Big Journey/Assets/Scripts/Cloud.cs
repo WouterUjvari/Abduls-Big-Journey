ae198d22001c52dfde1a4b34cf5e020640215cac
Dominik Beens <dominik.beens@hotmail.nl>
2017-11-03 13:42:14 +0100

    gave squid an item to attack with, game is showable, done wooo

--- Abduls Big Journey/Assets/Scripts/Cloud.cs
+++ Abduls Big Journey/Assets/Scripts/Cloud.cs
@@ -11,14 +11,16 @@ public class Cloud : MonoBehaviour {
     {
         
         if (other.gameObject.tag == "CloudOrange")
-        {          
-            gameObject.transform.position = cloudBlue.transform.GetChild(0).transform.position;
+        {
+            gameObject.transform.position = new Vector3(cloudBlue.transform.GetChild(0).transform.position.x, gameObject.transform.position.y, cloudBlue.transform.GetChild(0).transform.position.z); 
+            //gameObject.transform.position - new Vector3(cloudBlue.transform.GetChild(0).transform.position.x, gameObject.transform.position.y, cloudBlue.transform.GetChild(0).transform.position.z);
             
         }
 
         if (other.gameObject.tag == "CloudBlue")
         {
-            gameObject.transform.position = cloudOrange.transform.GetChild(0).transform.position;
+            //gameObject.transform.position = cloudOrange.transform.GetChild(0).transform.position;
+            gameObject.transform.position = new Vector3(cloudOrange.transform.GetChild(0).transform.position.x, gameObject.transform.position.y, cloudBlue.transform.GetChild(0).transform.position.z);
         }
     }
 
